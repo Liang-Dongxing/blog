f9ea1ea49e1fe3b3612bf6741aeb2c0890ad5fa1
LiangDongXing <liangdongxing.cn@outlook.com>
2021-11-05 15:45:09 +0800

    init

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,7 +2,7 @@ module.exports = {
     lang: 'zh-CN',
     title: 'Liang-DongXing',
     description: '这是我的第一个 VuePress 站点',
-    base: '/',
+    base: '/my-docs/',
     themeConfig: {
         logo: 'https://vuejs.org/images/logo.png',
         docsDir: 'docs',
